c69b12267a7f2108effeb31b35e559ad1f609c72
lindvi <charlie.lindviken@valtech.se>
2014-11-11 09:21:08 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Valhall::Application.config.secret_key_base = '7ab43b6d3e7459a9890a49408340152fbf851fa86114d1585a161ac070cf53d0e45192fa53181d2c14c907121881d3d7c01ce248200df05dfa0fb131f07de506'
